30d76749b62f4169055b8b3ecd3ee45a92934730
William Aditama <aditamawilliam@gmail.com>
2020-08-14 07:36:40 -0700

    Fixed gitlink

---
+++ covid-mobile/App.js
@@ -0,0 +1,33 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import MainScreen from './screens/MainScreen';
+import { AsyncStorage, ActivityIndicator } from 'react-native';
+
+import { v4 as uuidv4 } from 'uuid';
+
+export default function App() {
+  const [isLoading, setIsLoading] = React.useState(true);
+  userSetup().then(() => setIsLoading(false));
+
+  if (isLoading)
+    return <ActivityIndicator />
+  return (
+    <MainScreen />
+  );
+}
+
+
+async function userSetup() {
+  // Store new user if not created already
+  let storedID = await AsyncStorage.getItem('userId');
+  if (storedID === null) {
+    await AsyncStorage.setItem('userId', uuidv4());
+  }
+
+  let isUserIdSent = await AsyncStorage.getItem('userIdSent');
+  if (isUserIdSent === null) {
+    let res = await fetch(); // TODO Send userID to server
+    // TODO If res === HTTP OK
+    await AsyncStorage.setItem('userIdSent', 'true');
+  }
+}
