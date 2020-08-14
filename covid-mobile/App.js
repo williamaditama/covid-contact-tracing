bcac53b3dee92a1f737fc6aee471c9a4b8341e2e
William Aditama <aditamawilliam@gmail.com>
2020-08-14 08:58:23 -0700

    Front end update

--- covid-mobile/App.js
+++ covid-mobile/App.js
@@ -1,4 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import MainScreen from './screens/MainScreen';
 import { AsyncStorage, ActivityIndicator } from 'react-native';
