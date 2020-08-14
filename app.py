0140bed8e5b2047117bae5e86a4e1198fdcf9b76
Madguy16s <jason.gumarjadi@gmail.com>
2020-08-14 20:35:54 +0700

    Add RESTful API

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask
+from flask_restful import Api, Resource, reqparse, abort
+
+app = Flask(__name__)
+api = Api(app)
+
+userid_put_args = reqparse.RequestParser()
+userid_put_args.add_argument("userID", type=str, help="User Uniqe ID")
+
+user = {}
+
+
+# error handling
+def abort_if_userid_null(user_id):
+    if user_id not in user:
+        abort(404, message="UserId invalid")
+
+
+def abort_if_userid_duplicate(user_id):
+    if user_id in user:
+        abort(409, message="userID already exist")
+
+
+# userID class with get an put function
+class UserId(Resource):
+    def get(self, user_id):
+        abort_if_userid_duplicate(user_id)
+        abort_if_userid_null(user_id)
+        return user[user_id]
+
+    def put(self, user_id):
+        args = userid_put_args.parse_args()
+        user[user_id] = args
+        return user[user_id], 201
+
+
+
+api.add_resource(UserId, "/userid/<string:user_id>")
+
+if __name__ == '__main__':
+    app.run(debug=True)
