6663f4e06ff2f47d4e6ff02c9330979834cc2e4a
William Aditama <aditamawilliam@gmail.com>
2020-08-15 09:33:53 -0700

    Finished app

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ import { AsyncStorage, ActivityIndicator } from 'react-native';
 
 import { v4 as uuidv4 } from 'uuid';
 
+import { postUser } from './services/httpRequests';
+
 export default function App() {
   const [isLoading, setIsLoading] = React.useState(true);
   userSetup()
@@ -22,12 +24,13 @@ async function userSetup() {
   // Store new user if not created already
   let storedID = await AsyncStorage.getItem('userId');
   if (storedID === null) {
-    await AsyncStorage.setItem('userId', uuidv4());
+    let userID = uuidv4();
+    await AsyncStorage.setItem('userId', userID);
   }
 
   let isUserIdSent = await AsyncStorage.getItem('userIdSent');
   if (isUserIdSent === null) {
-    let res = await fetch(); // TODO Send userID to server
+    let res = await postUser(userID); // TODO Send userID to server
     // TODO If res === HTTP OK
     await AsyncStorage.setItem('userIdSent', 'true');
   }
--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,22 +1,76 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import MapView from 'react-native-maps'
+import { View, Text, StyleSheet, ActivityIndicator } from 'react-native';
+import MapView, { Marker } from 'react-native-maps'
+
+import * as Location from 'expo-location'
+
+import { getRiskLevel, postLocation } from '../services/httpRequests'
 
 
 export default function MainScreen(props) {
+    const [location, setLocation] = React.useState(null);
+    const [errorMsg, setErrorMsg] = React.useState(null);
+    const [riskLevel, setRiskLevel] = React.useState(null);
+
+
+
+    React.useEffect(() => {
+        (async () => {
+            let { status } = await Location.requestPermissionsAsync();
+            if (status !== 'granted') {
+                setErrorMsg('Permission to access location was denied');
+            }
+
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation(location);
+        })();
+    });
+
+
+    let loc = 'Waiting..';
+    if (errorMsg) {
+        loc = errorMsg;
+        return <View>
+            <Text>Cannot Load Location</Text>
+        </View>
+    } else if (location) {
+        loc = JSON.stringify(location);
+    } else {
+        return <View>
+            <ActivityIndicator />
+        </View>
+    }
+
+    let { latitude, longitude } = location.coords;
+    // if (riskLevel === null) {
+    getRiskLevel(latitude, longitude)
+        .then(data => data.risk)
+        .then(r => setRiskLevel(r))
+    // }
+
+    postLocation(latitude, longitude)
+    .then(d => console.log(d));
 
     return <View style={styles.container}>
         <MapView style={styles.map}
             initialRegion={{
-                latitude: 6.2088,
-                longitude: -106.8456,
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
                 latitudeDelta: 0.0922,
                 longitudeDelta: 0.0421,
             }}
-        />
-        <Text>COVID Score:</Text>
+        >
+            <Marker
+                coordinate={location.coords}
+                image={require('../assets/my-location.png')}
+            />
+        </MapView>
+        <Text>Risk Level:</Text>
+        <Text>{riskLevel}</Text>
     </View>
 }
+
+
 
 const styles = StyleSheet.create({
     map: {
