973493c4cfe6f8662c701f30f1b3eb42a82f45fc
ar2519 <ar2519@ic.ac.uk>
2020-08-15 11:02:15 +0100

    added rollback

--- backend/db_interface.py
+++ backend/db_interface.py
@@ -8,19 +8,26 @@ def connect():
 
 def add_user(uuid):
     connection = connect()
-    connection.cursor().execute('INSERT INTO users VALUES (%s)', (uuid,))
-    connection.commit()
+    try:
+        connection.cursor().execute('INSERT INTO users VALUES (%s)', (uuid,))
+        connection.commit()
+    except psycopg2.errors.InFailedSqlTransaction:
+        connection.rollback()
 
 def add_loc(userID, lat, lng, timestamp):
     connection = connect()
-    query = "INSERT INTO locations VALUES (%s, %s, %s, %s)"
-    connection.cursor().execute(query, (userID, lat, lng, timestamp))
-    connection.commit()
+    try:
+        query = "INSERT INTO locations VALUES (%s, %s, %s, %s)"
+        connection.cursor().execute(query, (userID, lat, lng, timestamp))
+        connection.commit()
+    except psycopg2.errors.InFailedSqlTransaction:
+        connection.rollback()
 
 def get_loc():
     connection = connect()
-
-    result = connection.execute('SELECT * FROM locations')
-    out = result.fetchall()
+    cursor = connection.cursor()
+    
+    cursor.execute('SELECT * FROM locations')
+    out = cursor.fetchall()
     #out is a list of tuples with each row = 1 tuple
     return out
